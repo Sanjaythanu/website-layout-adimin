e4dd8369fd60680c5f75dfc87b4e9abbaac3fe35
Sanjay Sri Thanu Malaiyaan <sanjaychittu2005@gmail.com>
2026-01-05 16:02:54 +0530

    wiggo

---
+++ myapp/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //  <h1>Bannari Amman Institute of Technology</h1>
+    //  <p>Bannari Amman Institute of Technology is an Autonomous, Self-financing Engineering College, Approved by AICTE, New Delhi and Affiliated to Anna University, Chennai. Nestled on the banks of The River Bhavani, BIT campus provides environment for learning in harmony with nature, away from the odds of the city life. The spacious and the earth hugging buildings punctuated with landscaped courtyards and pathways are designed to emphasise the business ethics and or characteristics of an excellent centre for learning. The Campus hosts well planned academic blocks, computer centres, lecture halls, libraries, laboratories, conference halls, staff quarters, hostels and students' centres. The Campus also houses a co-operative store, ATM (Axis, SBI and KVB) and a clinic to attend to the general health of the students and staff.</p>
+    // </div>
+    <>
+    <Home/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <h1> Bannari Amman Institute of Technology</h1>
+    <h2>Sanjay sri thanu malaiyaan </h2>
+    </>
+  )
+}
+
+export default Home
